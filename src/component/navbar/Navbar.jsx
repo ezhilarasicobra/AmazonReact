a64d34d041f000fb8df7c5e5ba7426acf8e15d49
Ezhilarasi <tezzhilarasi@gmail.com>
2021-12-02 22:35:03 +0530

    d

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import './Navbar.css'
 import axios from 'axios'
 import { DataContext } from '../../context/DataContext'
 
-const path='http://localhost:4001/'
+const path='https://vibrant-kare-98c436.netlify.app/'
 
 const color=['#a5e6a8','#ebbccc','#e6c0aa','#b2d1ed']
 
